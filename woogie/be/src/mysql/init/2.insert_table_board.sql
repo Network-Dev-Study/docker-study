499571368d6ad4b17db6face6ee076437407d222
woogieReal <woogiereal@gmail.com>
2023-11-03 16:23:26 +0900

    chore: db initial schema 및 data 추가

---
+++ woogie/be/src/mysql/init/2.insert_table_board.sql
@@ -0,0 +1,6 @@
+SET NAMES utf8 ;
+
+INSERT INTO `board` (`board_no`, `board_title`, `board_content`) VALUES
+(1, '첫 번째 게시글입니다.', '안녕하세요~!'),
+(2, '두 번째 게시글입니다.', '반갑습니다!'),
+(3, '세 번째 게시글입니다.', '굿모닝~');
