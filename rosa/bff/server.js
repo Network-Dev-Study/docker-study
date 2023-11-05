d9ffcfb81146ff6385453ce38b6ad380ff1fc511
ro-ssang <alakswk43@gmail.com>
2023-11-05 21:44:49 +0900

    [BFF] 게시판 CRUD API 생성

--- rosa/bff/server.js
+++ rosa/bff/server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/board', (req, res) => {
+  res.send('GET board list');
+});
+app.get('/board/:boardId', (req, res) => {
+  res.send('GET board detail');
+});
+app.post('/board', (req, res) => {
+  res.send('POST create board');
+});
+app.put('/board/:boardId', (req, res) => {
+  res.send('PUT update board');
+});
+app.delete('/board/:boardId', (req, res) => {
+  res.send('DELETE delete board');
 });
 
 app.listen(port, () => {
