c306ab059e44ccf1f4a893959cd98563be27418f
ro-ssang <alakswk43@gmail.com>
2023-11-05 23:05:30 +0900

    [BFF] 게시판 CRUD API 생성

--- rosa/bff/server.js
+++ rosa/bff/server.js
@@ -1,21 +1,42 @@
+const { default: axios } = require('axios');
 const express = require('express');
 const app = express();
 const port = 4000;
 
-app.get('/board', (req, res) => {
-  res.send('GET board list');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const loggerInstance = axios.create({ baseURL: 'http://localhost:4001' });
+const boardInstance = axios.create({ baseURL: 'http://localhost:4002' });
+
+app.get('/board', async (req, res) => {
+  await loggerInstance.post('/log', 'GET board list');
+  const result = await boardInstance.get(`/board?keyword=${req.query.keyword}&searchType=${req.query.searchType}`);
+  return res.send(result.data);
 });
-app.get('/board/:boardId', (req, res) => {
-  res.send('GET board detail');
+
+app.get('/board/:boardId', async (req, res) => {
+  await loggerInstance.post('/log', 'GET board detail');
+  const result = await boardInstance.get(`/board/${req.params.boardId}`);
+  return res.send(result.data);
 });
-app.post('/board', (req, res) => {
-  res.send('POST create board');
+
+app.post('/board', async (req, res) => {
+  await loggerInstance.post('/log', 'POST create board');
+  const result = await boardInstance.post('/board', req.body);
+  return res.send(result.data);
 });
-app.put('/board/:boardId', (req, res) => {
-  res.send('PUT update board');
+
+app.put('/board/:boardId', async (req, res) => {
+  await loggerInstance.post('/log', 'PUT update board');
+  const result = await boardInstance.put(`/board/${req.params.boardId}`, req.body);
+  return res.send(result.data);
 });
-app.delete('/board/:boardId', (req, res) => {
-  res.send('DELETE delete board');
+
+app.delete('/board/:boardId', async (req, res) => {
+  await loggerInstance.post('/log', 'DELETE delete board');
+  const result = await boardInstance.delete(`/board/${req.params.boardId}`);
+  return res.send(result.data);
 });
 
 app.listen(port, () => {
