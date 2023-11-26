e8a46a16df3bf6e57223914906f0b30f6da3f268
ro-ssang <alakswk43@gmail.com>
2023-11-27 06:02:09 +0900

    [Docker] prod 모드에서만 헬스 체크하도록 변경

--- rosa/scripts/prod-start.sh
+++ rosa/scripts/prod-start.sh
@@ -2,4 +2,4 @@
 source "$(pwd)/scripts/create-data-directory.sh"
 
 # docker compose 실행
-docker-compose -f ./docker-compose.yaml up -d
+docker-compose -f ./docker-compose.yaml -f ./docker-compose-prod.yaml up -d
